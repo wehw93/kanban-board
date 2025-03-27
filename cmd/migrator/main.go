ba46961fe8cbf767cb5edb4807f09de0077c901a
Egor <wehw93@mail.ru>
2025-03-27 22:41:28 +0300

    create http server

--- cmd/migrator/main.go
+++ cmd/migrator/main.go
@@ -5,11 +5,9 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"net/url"
-	"strings"
 
 	"github.com/golang-migrate/migrate/v4"
-	_"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/wehw93/kanban-board/internal/config"
 )
@@ -23,10 +21,7 @@ func main() {
 
 	cfg := config.MustLoad()
 
-	connURL, err := convertConnStringToURL(cfg.DB.GetDSN())
-	if err != nil {
-		panic(err)
-	}
+	connURL := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s", cfg.DB.User, cfg.DB.Password, cfg.DB.Host, cfg.DB.Port, cfg.DB.Name, cfg.DB.Sslmode)
 
 	m, err := migrate.New("file://"+
 		migrationsPath,
@@ -44,36 +39,4 @@ func main() {
 	}
 	fmt.Println("migrations applied succesfully")
 
-}
-
-// convertConnStringToURL преобразует формат "host=... port=..." в URL "postgres://..."
-func convertConnStringToURL(connStr string) (string, error) {
-	parts := strings.Fields(connStr)
-	params := make(map[string]string)
-
-	for _, part := range parts {
-		keyValue := strings.SplitN(part, "=", 2)
-		if len(keyValue) != 2 {
-			return "", fmt.Errorf("invalid connection string format")
-		}
-		params[keyValue[0]] = keyValue[1]
-	}
-
-	requiredKeys := []string{"host", "port", "dbname", "user", "password", "sslmode"}
-	for _, key := range requiredKeys {
-		if _, ok := params[key]; !ok {
-			return "", fmt.Errorf("missing required key: %s", key)
-		}
-	}
-
-	urlStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s",
-		url.QueryEscape(params["user"]),
-		url.QueryEscape(params["password"]),
-		params["host"],
-		params["port"],
-		params["dbname"],
-		params["sslmode"],
-	)
-
-	return urlStr, nil
 }
